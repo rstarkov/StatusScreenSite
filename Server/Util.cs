1049b83e2b4df91df86b818aaba8bc442fc5d8f3
rstarkov <sourcecode@roman.st>
2017-08-13 00:13:12 +0100

    Tidy up

--- Server/Util.cs
+++ Server/Util.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace StatusScreenSite
 {
     static class Util
     {
-        private static object start;
-
         public static void SleepUntil(DateTime time)
         {
             var duration = time.ToUniversalTime() - DateTime.UtcNow;
