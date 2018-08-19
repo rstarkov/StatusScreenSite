4bc447c9923904dc30486a5968dabe976cadda54
rstarkov <sourcecode@roman.st>
2018-08-19 22:37:54 +0100

    Httping: hourly chart; messy and incomplete support for switching between linear / log scales and uptime / response time plots.

--- Client/Src/Services/HttpingService.ts
+++ Client/Src/Services/HttpingService.ts
@@ -23,6 +23,7 @@ export class HttpingService extends Service {
                         <th colspan=3>30 days</th>
                         <th rowspan=2>Recent chart</th>
                         <th rowspan=2>2 min chart</th>
+                        <th rowspan=2>Hourly chart</th>
                         <th rowspan=2>Daily chart</th>
                     </tr>
                     <tr>
@@ -84,6 +85,7 @@ class Entry {
     private $td30dErr: Util.Html;
     private $tdChartRecent: Util.Html;
     private $tdChart2min: Util.Html;
+    private $tdChartHourly: Util.Html;
     private $tdChartDaily: Util.Html;
     private _greenMsCutoff: number;
     private _redMsCutoff: number;
@@ -91,6 +93,8 @@ class Entry {
     private _dataRecent: Plottable.Dataset;
     private _plot2min: Plottable.Plot;
     private _data2min: Datasets;
+    private _plotHourly: Plottable.Plot;
+    private _dataHourly: Datasets;
     private _plotDaily: Plottable.Plot;
     private _dataDaily: Datasets;
 
@@ -107,6 +111,7 @@ class Entry {
                 <td id=hp30dErr class=groupleft></td>
                 <td id=hpChartRecent class="plot recent"><div class="plot recent"></div></td>
                 <td id=hpChart2min class="plot 2min"><div class="plot 2min"></div></td>
+                <td id=hpChartHourly class="plot hourly"><div class="plot hourly"></div></td>
                 <td id=hpChartDaily class="plot daily"><div class="plot daily"></div></td>
             </tr>
         `);
@@ -121,6 +126,7 @@ class Entry {
         this.$td30dErr = Util.$get(this.$row, 'td#hp30dErr');
         this.$tdChartRecent = Util.$get(this.$row, 'td#hpChartRecent');
         this.$tdChart2min = Util.$get(this.$row, 'td#hpChart2min');
+        this.$tdChartHourly = Util.$get(this.$row, 'td#hpChartHourly');
         this.$tdChartDaily = Util.$get(this.$row, 'td#hpChartDaily');
 
         this._dataRecent = null;
@@ -162,14 +168,24 @@ class Entry {
         this.$td30dErr.html(`${downtime30d.toFixed(2)}%`).css('color', downtime30d < 0.05 ? Colors.GreenText : downtime30d > 0.5 ? Colors.RedText : Colors.BlueText);
 
         this._dataRecent.data(dto.Recent);
-        this._data2min.prc01.data(dto.Twominutely);
-        this._data2min.prc50.data(dto.Twominutely);
-        this._data2min.prc75.data(dto.Twominutely);
-        this._data2min.prc95.data(dto.Twominutely);
-        this._dataDaily.prc01.data(dto.Daily);
-        this._dataDaily.prc50.data(dto.Daily);
-        this._dataDaily.prc75.data(dto.Daily);
-        this._dataDaily.prc95.data(dto.Daily);
+        if (this._data2min) {
+            this._data2min.prc01.data(dto.Twominutely);
+            this._data2min.prc50.data(dto.Twominutely);
+            this._data2min.prc75.data(dto.Twominutely);
+            this._data2min.prc95.data(dto.Twominutely);
+        }
+        if (this._dataHourly) {
+            this._dataHourly.prc01.data(dto.Hourly);
+            this._dataHourly.prc50.data(dto.Hourly);
+            this._dataHourly.prc75.data(dto.Hourly);
+            this._dataHourly.prc95.data(dto.Hourly);
+        }
+        if (this._dataDaily) {
+            this._dataDaily.prc01.data(dto.Daily);
+            this._dataDaily.prc50.data(dto.Daily);
+            this._dataDaily.prc75.data(dto.Daily);
+            this._dataDaily.prc95.data(dto.Daily);
+        }
     }
 
     public Redraw(): void {
@@ -177,6 +193,8 @@ class Entry {
             this._plotRecent.redraw();
         if (this._plot2min)
             this._plot2min.redraw();
+        if (this._plotHourly)
+            this._plotHourly.redraw();
         if (this._plotDaily)
             this._plotDaily.redraw();
     }
@@ -186,7 +204,11 @@ class Entry {
         if (this._greenMsCutoff > 5000)
             this._greenMsCutoff = 5000;
         this._redMsCutoff = this._greenMsCutoff * 1.5;
-        let yScale = new Plottable.Scales.ModifiedLog(10).domainMin(dto.Last30d.MsResponsePrc01 * 0.9).domainMax(dto.Last30d.MsResponsePrc50 * 3);
+        let yScale: Plottable.Scale<{}, number>;
+        if (false)
+            yScale = new Plottable.Scales.Linear().domainMin(0).domainMax(1000);
+        else
+            yScale = new Plottable.Scales.ModifiedLog(10).domainMin(dto.Last30d.MsResponsePrc01 * 0.9).domainMax(dto.Last30d.MsResponsePrc50 * 3)
 
         // Recent chart
         {
@@ -203,9 +225,42 @@ class Entry {
                 .renderTo(<any>d3.select(Util.get(this.$tdChartRecent, 'div.plot')));
         }
 
-        // Two min & daily chart
-        [this._data2min, this._plot2min] = this.initStackedPlot(Util.get(this.$tdChart2min, 'div.plot'), yScale);
-        [this._dataDaily, this._plotDaily] = this.initStackedPlot(Util.get(this.$tdChartDaily, 'div.plot'), yScale);
+        // Two min, hourly & daily chart
+        if (false) {
+            yScale = new Plottable.Scales.Linear().domainMin(0).domainMax(101);
+            yScale = new Plottable.Scales.ModifiedLog(10).domainMin(0).domainMax(101);
+            [this._data2min, this._plot2min] = this.initUptimePlot(Util.get(this.$tdChart2min, 'div.plot'), yScale);
+            [this._dataHourly, this._plotHourly] = this.initUptimePlot(Util.get(this.$tdChartHourly, 'div.plot'), yScale);
+            [this._dataDaily, this._plotDaily] = this.initUptimePlot(Util.get(this.$tdChartDaily, 'div.plot'), yScale);
+        }
+        else {
+            [this._data2min, this._plot2min] = this.initStackedPlot(Util.get(this.$tdChart2min, 'div.plot'), yScale);
+            [this._dataHourly, this._plotHourly] = this.initStackedPlot(Util.get(this.$tdChartHourly, 'div.plot'), yScale);
+            [this._dataDaily, this._plotDaily] = this.initStackedPlot(Util.get(this.$tdChartDaily, 'div.plot'), yScale);
+        }
+    }
+
+    private initUptimePlot(target: HTMLElement, yScale: Plottable.Scale<{}, number>): [Datasets, Plottable.Plot] {
+        var datas = {
+            prc01: new Plottable.Dataset([], { type: null, color: "green" }),
+            prc50: new Plottable.Dataset([], { type: "ErrorCount", color: "yellow" }),
+            prc75: new Plottable.Dataset([], { type: "TimeoutCount", color: "red" }),
+            prc95: new Plottable.Dataset([], { type: "DUMMY", color: "blue" }),
+            // FIXME!
+        };
+        let xScale = new Plottable.Scales.Linear().domainMin(-1).domainMax(30);
+        let colorScale = new Plottable.Scales.Color()
+            .domain(["green", "yellow", "blue", "red", "fuchsia", "grey"])
+            .range([Colors.GreenBar, Colors.YellowBar, Colors.BlueBar, Colors.RedBar, Colors.FuchsiaBar, Colors.GreyBar]);
+        var plot = new Plottable.Plots.StackedBar()
+            .addDataset(datas.prc75)
+            .addDataset(datas.prc50)
+            .addDataset(datas.prc01)
+            .x((pt, i, ds) => { return i; }, xScale)
+            .y((pt, i, ds) => { return pt.TotalCount == 0 ? (ds.metadata().type ? 0 : 100) : ((ds.metadata().type ? pt[ds.metadata().type] : (pt.TotalCount - pt.ErrorCount - pt.TimeoutCount)) / pt.TotalCount * 100); }, yScale)
+            .attr('fill', (pt, i, ds) => { return pt.TotalCount == 0 ? "grey" : ds.metadata().color; }, colorScale)
+            .renderTo(<any>d3.select(target));
+        return [datas, plot];
     }
 
     private initStackedPlot(target: HTMLElement, yScale: Plottable.Scale<{}, number>): [Datasets, Plottable.Plot] {
